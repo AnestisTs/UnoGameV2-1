f7f531bb47b3de595bdf8dc01f76fda9b9955454
anestis <anestis.tsolakidis@live.de>
2023-08-14 21:44:01 +0300

    comments in unogame.kt are now where they should be.

--- src/main/kotlin/UnoGame.kt
+++ src/main/kotlin/UnoGame.kt
@@ -2,9 +2,17 @@ import java.util.*
 
 class UnoGame(var numberPlayers: Int, var playerNames: MutableList<String>){
 
+    var currentCard: Card = Card(CardColor.BLUE, CardValue.FIVE)   // * Dummy wert, der direkt überschrieben wird                              // * Ist die gelegte Karte auf dem Stapel, die eine Aktion erfordert vom nächsten Spieler. ? = null fängt einen möglich crash ab für einen jetzt nicht existierenden Wert. // ? null wert eingebaut arbeite aber nicht mit 0 evlt try catch stattdessen benutzen?
+    var stack = mutableListOf<Card>()                           // * Stack ist der Kartenstapel, der gelegten Karten und wird mit Card befüllt
+    var gameOver: Boolean = false                                   // *  standartmäßiug falsch gesetzt, da dass Spiel fortlaufend ist
+    var currentPlayer: Int = 0                                                          // * standartmäßig auf 0 gesetzt um den Spieler zu initialisieren
+    var players : MutableList<Player> = mutableListOf()
+    val deck: MutableList<Card> = mutableListOf()                                                   // * variable fürs deck erstellt mit einer mutablelist der Klasse Card als Datentyp.
 
+    var clockwisePlayerTurns: Boolean = true
 
-    init {                                                      // *  initialisierung vom Start des Kartenspiels
+    // *  initialisierung vom Start des Kartenspiels
+    init {
 
 
         // * hier läuft die schleife für jede Farbe durch
@@ -39,7 +47,8 @@ class UnoGame(var numberPlayers: Int, var playerNames: MutableList<String>){
                 player.playerHand.hand.add(deck.removeAt(0))
 
             }
-            players.add(player)                                // * die Spieler werden mit players.add(player) zur Liste players hinzugefügt und somit Teil der Liste der Spielerhände.
+            // * die Spieler werden mit players.add(player) zur Liste players hinzugefügt und somit Teil der Liste der Spielerhände.
+            players.add(player)
 
         }
 
@@ -81,7 +90,7 @@ class UnoGame(var numberPlayers: Int, var playerNames: MutableList<String>){
             } else if (currentCard is ReverseCard){
                 clockwisePlayerTurns = false
             }else if (currentCard is SkipCard){
-                currentPlayer = getNextPlayerIndex()                        // ! passt noch nicht ganz, da hier der zweite spieler übersprungen wird...
+                currentPlayer = getNextPlayerIndex() + 1
             }
 
             // *  mit playerhands.get(currentplayer) wird das deck des momentanen spielers angezeigt. .withindex fügt die information des Index hinzu.
@@ -103,27 +112,39 @@ class UnoGame(var numberPlayers: Int, var playerNames: MutableList<String>){
                     println("Du hast nur ${playerHand.hand.size-1} Karten auf der Hand, mach eine vernünftige Eingabe.")
                     return startGame()
                 }
-                var chosenCard = playerHand.hand.elementAt(chosenCardIndex)                                                  // ? Ausgewählte Karte vom Spieler aus seiner Hand über Index
+                // * Ausgewählte Karte vom Spieler aus seiner Hand über Index
+                var chosenCard = playerHand.hand.elementAt(chosenCardIndex)
                 if (chosenCard.color == currentCard.color || chosenCard.value == currentCard.value) {
-                    if (chosenCard is SkipCard){                                                                        // ! ELSE IF einbauen    // ! IS durch chatgpt erlernt. Hätte ich das nicht gemacht
-                        currentPlayer = chosenCard.skipPlayer(clockwisePlayerTurns, currentPlayer, numberPlayers)                     // ! könnte ich skipPlayer nicht benutzen
+                   // * chosencard wird hier als Skipcard deklariert. durch "is" werden die eigenschaften übernommen. // * Baue damit ein interface auf.
+                    // ! IS durch chatgpt erlernt. Hätte ich das nicht gemacht, könnte ich skipPlayer nicht benutzen
+                    if (chosenCard is SkipCard){
+                         // * überspringt den Spieler, auf den die Karte gelegt wurde.
+                        currentPlayer = chosenCard.skipPlayer(clockwisePlayerTurns, currentPlayer, numberPlayers)
                     }
-                    else if (chosenCard is ReverseCard){                                                             // * Baue damit ein interface auf. Es werden die eigenschaften übernommen
+                    else if (chosenCard is ReverseCard){
                         clockwisePlayerTurns = chosenCard.reverseIt(clockwisePlayerTurns)
                     }
+                   // * hier wird die vom user ausgewählte karte zum Kartenstapel (Stack) hinzugefügt.
                     stack.add(chosenCard)
-                    currentCard = chosenCard                                                                                // * currengcard als chosencard deklariert um die aktuelle karte auf dem stapel aufzuzeigen
-                    playerHand.hand.remove(chosenCard)                // * Karte wird zum Stapel hinzugefügt und aus der Spielerhand entnommen
+                    // * currengcard als chosencard deklariert um die aktuelle karte auf dem stapel aufzuzeigen
+                    currentCard = chosenCard
+                    // * die vom user ausgewählte karte wird aus der Spielerhand entfernt.
+                    playerHand.hand.remove(chosenCard)
                 }
             } else {
                 println("bitte ziehe eine Karte")
+                // * wenn die bedingungen von isPlayable nicht erfüllt werden, wird der Spieler gebeten eine Karte zu ziehen und tut dies.
+                // *  Die funktion hierfür ist in PlayerHand.kt >>>
                 playerHand.drawCard(deck, stack, currentCard)
 
             }
 
+            // * Spielrichtung im Uhrzeiger SInn
             currentPlayer = getNextPlayerIndex()
+            // * wenn die Karten auf der Hand leer sind, wird das Spiel beendet.
             if (playerHand.hand.size == 0){
-                gameOver = true                         // ! Spiel wieder von neu anfangen, karten müssen neu ausgeteilt werden
+                gameOver = true
+                // * Der Gewinner wird hier angezeigt.
                 println("Spieler ${player.name} hat gewonnen. Herzlichen glückwunsch, du bist ein noob.")
 
             }
