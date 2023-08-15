9dcebf2940685265baf5ce6be5ed9f758ec52ef4
anestis <anestis.tsolakidis@live.de>
2023-08-15 23:17:17 +0300

    updated TODO's

--- src/main/kotlin/TODO.kt
+++ src/main/kotlin/TODO.kt
@@ -8,18 +8,18 @@
 // ! Feedback falls falsche Kartenlegung *evlt die Person nächste Runde überspringen* / INFO
 // ? Kosmetische Verbesserung Wert. Vor KartenWert schreiben
 // * Kosmetische verbesserung. Kommentare über den Code schreiben *DONE*
-// *  Player klasse erstellen statt in der UnoGame.kt bsp name hand. evtl punkte Stand Mehr funktionen ( kampschrei ) *DONE*
-// ! stats funktion
-// * user / KIuser erbt von player *PLAYER&NPCPLAYER in meinem fall. 70%
-// * IF Abfrage für die Eingabe des Indexes der Maximalen Karten auf der Hand bei Zeile 96? Chosecard *DONE*
+// * Player klasse erstellen statt in der UnoGame.kt bsp name hand. evtl punkte Stand Mehr funktionen ( kampschrei ) *DONE*
+// ! stats funktion *keine momentane verwendung*
+// * user / KIuser erbt von player *PLAYER&NPCPLAYER in meinem fall. *DONE*
+// * IF Abfrage für die Eingabe des Indexes der Maximalen Karten auf der Hand. Chosecard *DONE*
 // * ab einem gewissen Punkt, wird in endlos schleife ein fehler ausgeworfen, der mich immer wieder karten ziehen lässt und alle runden überpsringt. *DONE*
 // *Lag an der drawCard funktion..*
 // ? deck & stack als klassen ... Auf später verschoben, einfach zum spaß das Spiel bearbeiten.
 // * FIXED reverse als erste karte führt nicht den geweünschten effekt aus
-// ! skip als erste karte führt nicht den geweünschten effekt aus
-// ! TODO NOW! npcplayer class erbend vom player erstellen mit spiellogic vom normalem game ! //
-// ! ^statt readln mit if else arbeiten
-// ! evlt karten nicht sichtbar machen
+// * skip als erste karte führt nicht den geweünschten effekt aus *DONE*
+// ! TODO NOW! npcplayer class erbend vom player erstellen mit spiellogic vom normalem game ! // *DONE*
+// * ^statt readln mit if else arbeiten *DONE*
+// ! evlt karten nicht sichtbar machen *FUTURE ADD*
 
 // ! TODO in near feature if enough time/after fulfilling project requierments. ! //
 // ! TODO add class DECK&STACK for project improvement ! //
